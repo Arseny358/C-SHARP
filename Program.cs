b04818901c74ae9b32825e291c8b0a88e0fc2faa
Arseny358 <arsenijomelcenko26@gmail.com>
2025-02-19 21:42:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _6_задание
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int N;
+
+            if (!int.TryParse(Console.ReadLine(), out N))
+            {
+                Console.WriteLine("Неверный ввод");
+                return;
+            }
+            bool D = true;
+            if (N >= 10)
+            {
+                int F = N % 10;
+                N /= 10;
+                while (N > 0)
+                {
+                    if (N % 10 != F)
+                    {
+                        D = false;
+                        break;
+                    }
+                    N /= 10;
+                }
+            }
+            if (D)
+            {
+                Console.WriteLine("Все цифры одинаковые");
+            }
+            else
+            {
+                Console.WriteLine("Не все цифры одинаковые");
+            }
+        }
+    }
+}
