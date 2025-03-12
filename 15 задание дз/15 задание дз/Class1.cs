194f3f8a673de04ca0214bb69d91023950ea6c64
Arseny358 <arsenijomelcenko26@gmail.com>
2025-03-12 18:12:34 +0300

    Add files via upload

---
+++ 15 задание дз/15 задание дз/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _15_задание_дз
+{
+    class Class1
+    {
+        public static int Chetn_sum(int[] numbers)
+        {
+            int s = 0;
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0 && numbers[i] > 0)
+                {
+                    s += numbers[i];
+                    count++;
+                }
+            }
+            if (count > 0)
+                return s / count;
+            else
+                return 0;
+        }
+    }
+}
---
+++ 15 задание дз/15 задание дз/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _15_задание_дз
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] Ar = { 1, 2, 3, 4, 5 };
+            if (Class1.Chetn_sum(Ar) == 3)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+            int[] Ar1 = {-1, -2, -3, -4, -5};
+            if (Class1.Chetn_sum(Ar1) == 0)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+            int[] Ar2 = {1, -2, 3, 4, 5, 6};
+            if (Class1.Chetn_sum(Ar2) == 5)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+            int[] Ar3 = {-1, -2, -3, -4, -5, 6};
+            if (Class1.Chetn_sum(Ar3) == 6)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+            int[] Ar4 = {-3};
+            if (Class1.Chetn_sum(Ar4) == 0)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
