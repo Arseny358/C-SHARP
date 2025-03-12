405060aa072a1ef7e8136b16a7f8815964da7a73
Arseny358 <arsenijomelcenko26@gmail.com>
2025-03-12 18:12:53 +0300

    Add files via upload

---
+++ 16 задание дз/16 задание дз/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _16_задание_дз
+{
+    class Program
+    {
+        public static int NOD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int c = b;
+                b = a % b;
+                a = c;
+            }
+            return a;
+        }
+        public static void Main(string[] args)
+        {
+            if (NOD(12, 18) == 6)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+            if (NOD(1, 2) == 1)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+            if (NOD(3, 4) == 1)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+            if (NOD(36, 36) == 36)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+            if (NOD(225, 25) == 25)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+            if (NOD(64, 18) == 2)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+            if (NOD(20, 18) == 2)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+        }
+    }
+}
---
+++ 16 задание дз/16 задание дз/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("16 задание дз")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("16 задание дз")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7f47a8ae-0f0f-4d1a-96d5-be6a65511eb3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
