f9f8780bcaab00fb24e2b906003a512dd0ee1682
Arseny358 <arsenijomelcenko26@gmail.com>
2025-03-12 18:11:51 +0300

    Add files via upload

---
+++ 14 задание дз/14 задание дз/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _14_задание_дз
+{
+    class Summ_nubers_number
+    {
+        public static int Test_snumb(int number)
+        {
+            int s = 0;
+            while (number > 0)
+            {
+                s += number % 10;
+                number /= 10;
+            }
+            return s;
+        }
+    }
+}
---
+++ 14 задание дз/14 задание дз/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _14_задание_дз
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (Summ_nubers_number.Test_snumb(123) == 6)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+            if (Summ_nubers_number.Test_snumb(12) == 3)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+            if (Summ_nubers_number.Test_snumb(1) == 1)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+            if (Summ_nubers_number.Test_snumb(10000000) == 1)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+            if (Summ_nubers_number.Test_snumb(345) == 12)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+            if (Summ_nubers_number.Test_snumb(0) == 0)
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Error");
+        }
+    }
+}
